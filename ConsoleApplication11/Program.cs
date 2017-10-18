8376b0a863906fcebb82d7e878d44a20e2d64c30
yinzidefengmeng <947231885@qq.com>
2017-10-18 21:08:13 +0800

    计算器2

---
+++ ConsoleApplication11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+        }
+    }
+}
